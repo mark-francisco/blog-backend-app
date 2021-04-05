e7682497f5925d7a404a2f4854b28e755228a034
Mark Francisco <mfrancisco581@gmail.com>
2021-04-05 09:56:01 -0500

    Fix v-for in index posts

--- src/views/PostsIndex.vue
+++ src/views/PostsIndex.vue
@@ -2,6 +2,7 @@
   <div class="posts-index">
     <div class="card" style="width: 18rem">
       <ul v-for="post in posts" v-bind:key="post.id">
+        <!-- pre-pending your routes with "/" ensures that you start at the root route -->
         <router-link v-bind:to="`/posts/${post.id}`">
           <img v-bind:src="post.image" class="card-img-top" alt="..." />
         </router-link>
@@ -10,17 +11,6 @@
         </div>
       </ul>
     </div>
-
-    <!-- <ul v-for="post in posts" v-bind:key="post.id">
-      <hr />
-      <h2>{{ post.id }}</h2>
-      <h2>{{ post.title }}</h2>
-      pre-pending your routes with "/" ensures that you start at the root route -->
-    <!-- <router-link v-bind:to="`/posts/${post.id}`">
-        <img v-bind:src="post.image" />
-      </router-link>
-      <hr />
-    </ul> -->
   </div>
 </template>
 
